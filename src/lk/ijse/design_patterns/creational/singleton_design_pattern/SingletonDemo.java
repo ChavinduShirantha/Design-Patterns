5f743caf78ce34e7107f928a90785945a0545398
ChavinduShirantha <chavindushirantha@gmail.com>
2023-12-02 13:29:49 +0530

    Initial Commit

---
+++ src/lk/ijse/design_patterns/creational/singleton_design_pattern/SingletonDemo.java
@@ -0,0 +1,9 @@
+package lk.ijse.design_patterns.creational.singleton_design_pattern;
+
+/**
+ * @author : Chavindu
+ * created : 12/2/2023-1:28 PM
+ **/
+
+public class SingletonDemo {
+}
---
+++ src/lk/ijse/design_patterns/creational/singleton_design_pattern/SingletonObject.java
@@ -0,0 +1,9 @@
+package lk.ijse.design_patterns.creational.singleton_design_pattern;
+
+/**
+ * @author : Chavindu
+ * created : 12/2/2023-1:29 PM
+ **/
+
+public class SingletonObject {
+}
